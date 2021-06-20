a261a6923cf9247df7f0801eae08b4d2d2daf3d9
Crowdbotics <team@crowdbotics.com>
2021-06-20 02:22:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shiny_smoke_28108/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shiny_smoke_28108;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shiny_smoke_28108";
+  }
+}
